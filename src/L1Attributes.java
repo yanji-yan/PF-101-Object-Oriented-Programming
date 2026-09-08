17a089dac9bda20bf3639251481557031c8d5d55
Geoffrey Jheanne <geoffreyjheanne0521@gmail.com>
2026-09-08 20:11:55 +0800

    Add files via upload

---
+++ src/L1Attributes.java
@@ -0,0 +1,14 @@
+public class L1Attributes {
+    String fname = "Geoffrey Jheanne";
+    String lname = "Dollentas";
+    String course = "BS Information Technology";
+    String major = "Instructional Systems Technology";
+    int age = 18;
+
+    public static void main(String[] args) {
+        L1Attributes myObj = new L1Attributes();
+        System.out.println("Name:" + myObj.fname + " " + myObj.lname);
+        System.out.println("Age: " + myObj.age);
+
+    }
+}
