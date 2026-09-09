5214c75f4c867deb8b68fa3ef7ff27be88356fe0
Geoffrey Jheanne <geoffreyjheanne0521@gmail.com>
2026-09-09 20:37:43 +0800

    Add files via upload

---
+++ src/L2FullThrottle.java
@@ -0,0 +1,19 @@
+public class L2FullThrottle {
+    static void fullThrottle(){
+        System.out.println("The car is going as fast as it can!");
+
+    }
+    static void speed(int maxSpeed) {
+        System.out.println("Max speed: " + maxSpeed);
+
+    }
+
+    public static void main(String[] args) {
+
+        fullThrottle();
+        speed(200);
+
+    }
+}
+
+
