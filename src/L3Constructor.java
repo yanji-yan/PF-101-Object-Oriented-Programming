02b57a5687b34e95da6143b85018c5184dc5590f
Geoffrey Jheanne <geoffreyjheanne0521@gmail.com>
2026-09-11 07:55:41 +0800

    constructor java

---
+++ src/L3Constructor.java
@@ -0,0 +1,12 @@
+public class L3Constructor {
+int x;
+
+public L3Constructor() {
+    x = 5;
+}
+
+    public static void main(String[] args) {
+        L3Constructor myObj = new L3Constructor();
+        System.out.println(myObj.x);
+    }
+}
