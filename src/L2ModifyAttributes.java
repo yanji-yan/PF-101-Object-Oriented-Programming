c506676690e4869e3caab5fe9306576b316edc7b
Geoffrey Jheanne <geoffreyjheanne0521@gmail.com>
2026-09-15 09:33:36 +0800

    modifying attributes

---
+++ src/L2ModifyAttributes.java
@@ -0,0 +1,15 @@
+public class L2ModifyAttributes  {
+
+    int x;
+
+    public static void main(String[] args) {
+        L2ModifyAttributes obj = new L2ModifyAttributes();
+        obj.x = 67;
+        System.out.println(obj.x);
+
+        // set the value of all x to 67
+
+    }
+
+    }
+
