9d75dddb3d2d7097811bda4595ca3f1da0c87cc0
Geoffrey Jheanne <geoffreyjheanne0521@gmail.com>
2026-09-16 07:29:17 +0800

    overriding attributes example

---
+++ src/L2OverrideAttributes.java
@@ -0,0 +1,13 @@
+public class L2OverrideAttributes {
+
+    // Change the value of x to 25:
+    int x = 10;
+
+    public static void main(String[] args) {
+        L2OverrideAttributes myObj = new L2OverrideAttributes();
+        myObj.x = 25; // x is now 25
+        System.out.println(myObj.x);
+
+
+    }
+}
