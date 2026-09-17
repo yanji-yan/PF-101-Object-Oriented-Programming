c02fdea6e25dcd3e300a620e6c9e2ae1c2c19692
Geoffrey Jheanne <geoffreyjheanne0521@gmail.com>
2026-09-17 09:56:42 +0800

    Add files via upload

---
+++ src/L8MultipleAttributesModifiers.java
@@ -0,0 +1,13 @@
+public class L8MultipleAttributesModifiers {
+
+        String fname = "Geoffrey Jheanne";
+        String lname = "Dollentas";
+        int age = 19;
+
+        public static void main(String[] args) {
+            L8MultipleAttributesModifiers myObj = new L8MultipleAttributesModifiers();
+            System.out.println("Name: " + myObj.fname + " " + myObj.lname);  System.out.println("Age: " + myObj.age);
+        }
+    }
+
+
