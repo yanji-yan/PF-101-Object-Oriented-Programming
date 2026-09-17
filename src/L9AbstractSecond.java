99a8673affa77873744e5b755c3d35b6bc25b379
Geoffrey Jheanne <geoffreyjheanne0521@gmail.com>
2026-09-18 06:55:38 +0800

    abstract second

---
+++ src/L9AbstractSecond.java
@@ -0,0 +1,13 @@
+class L9AbstractSecond {
+
+    public static void main(String[] args) {
+        Student myoBj = new Student();
+
+        System.out.println("Name: " + myoBj.fname);
+        System.out.println("Age: " + myoBj.age);
+        System.out.println("Graduation Year: " + myoBj.graduationYear);
+        myoBj.study();
+
+    }
+
+}
