643d0f71ba0892775606b2c313301261058da6d2
Geoffrey Jheanne <geoffreyjheanne0521@gmail.com>
2026-09-19 14:18:58 +0800

    final

---
+++ src/L9Final.java
@@ -0,0 +1,13 @@
+public class L9Final {
+    int x = 10;
+    double PI = 3.14;
+
+    public static void main(String[] args) {
+        L9Final myObj = new L9Final();
+        myObj.x = 50;
+        myObj.PI = 11;
+        System.out.println(myObj.x);
+        System.out.println(myObj.PI);
+
+    }
+}
