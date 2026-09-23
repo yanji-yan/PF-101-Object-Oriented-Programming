798100129e85b4bcc1b6ab1797ca7ff7a698ac90
Geoffrey Jheanne <geoffreyjheanne0521@gmail.com>
2026-09-23 19:55:45 +0800

    fixed the final modifier
    Changed PI to a final variable to make it a constant.

--- src/L9Final.java
+++ src/L9Final.java
@@ -1,13 +1,12 @@
 public class L9Final {
     int x = 10;
-    double PI = 3.14;
+    final double PI = 3.14; // Added final keyword so PI acts as a constant
 
     public static void main(String[] args) {
         L9Final myObj = new L9Final();
-        myObj.x = 50;
-        myObj.PI = 11;
-        System.out.println(myObj.x);
-        System.out.println(myObj.PI);
+        myObj.x = 50; // Allowed: x is non-final
 
+        System.out.println(myObj.x);   // Outputs 50
+        System.out.println(myObj.PI);  // Outputs 3.14
     }
 }
