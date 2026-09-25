d232d3c4bf73ac6628662b51484c15049ecc4bb7
Geoffrey Jheanne <geoffreyjheanne0521@gmail.com>
2026-09-25 18:57:44 +0800

    second encap

---
+++ src/L11SecondEncapsulation.java
@@ -0,0 +1,9 @@
+public class L11SecondEncapsulation {
+
+    public static void main(String[] args) {
+        L11FirstEncapsulation myObj = new L11FirstEncapsulation();
+        myObj.setName("Yanji");
+        System.out.println(myObj.getName());
+
+    }
+}
